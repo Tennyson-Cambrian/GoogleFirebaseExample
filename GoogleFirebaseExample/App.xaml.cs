52666adfc872a0bebf06eb7d2f144beb3a5483b6
TENNYSON FAUBERT <tennyson.faubert@cambriancollege.ca>
2024-08-07 22:27:05 -0400

    Add project files.

---
+++ GoogleFirebaseExample/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GoogleFirebaseExample
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ GoogleFirebaseExample/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿
+
+
+using Firebase.Storage;
+
+namespace GoogleFirebaseExample
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            var photo = await MediaPicker.PickPhotoAsync();
+
+            if (photo == null)
+            {
+                return;
+            }
+            var task = new FirebaseStorage("class2024-777ce.appspot.com", new FirebaseStorageOptions { ThrowOnCancel = true}).Child(photo.FileName).PutAsync(await photo.OpenReadAsync());
+            task.Progress.ProgressChanged += (sender, e) => { progressBar.Progress = e.Percentage; };
+
+            var downloadlink = await task;
+            downloadLink.Text = downloadlink;
+
+
+        }
+    }
+
+}
